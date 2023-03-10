4edf407a14b5716063fd3ae010aad70dfebc9da4
Raí Raul <87578739+vbsravus@users.noreply.github.com>
2023-03-09 22:02:22 -0300

    Add files via upload

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,20 @@
+import './home.css'
+
 export default function Home(params) {
     return(
-        <div>
-            <div></div>
+        <div className="home-mainDiv-container">
+            <div className="home-headerTitleDiv-container">
+                <div className="home-LogoDiv-container">
+                    <img className='home-LogoIMG-tag' src={require('./assets/PrototypeMissionLogo.png')}/>
+                    <h1>BEM-VINDO AO PROTOTYPE MISSION</h1>
+                </div>
+            </div>
+            <div className="home-bodyDiv-container">
+
+            </div>
+            <div className="home-bottomDiv-container">
+
+            </div>
         </div>
     )
 }

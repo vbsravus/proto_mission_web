24a7b30ff7fdea6b64b69748ec140e7abb1d56d0
Raí Raul <87578739+vbsravus@users.noreply.github.com>
2023-03-18 19:21:56 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from './components/home/home'
 import Lolleague from './components/lolleague/lolleague'
 import NavBottomBar from './components/navigation_bottombar/navigation_bottombar'
-
+import Test from './test'
+import ReactGA from 'react-ga';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const TRACKING_ID = "G-N3TTHRE5ZC"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 root.render(
     <BrowserRouter>
       <NavBottomBar/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/br/thrive" element={<Lolleague />}></Route>
+        <Route path="/test" element={
+          <Test></Test>
+        }></Route>
       </Routes>
     </BrowserRouter>
 );
